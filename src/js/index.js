47ee30b293d188c0a20f95dd7a3232b49365bccf
Christiano0407 <placebo0704@gmail.com>
2023-04-10 09:09:38 -0600

    add new aspects dev video player

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
 //** === Video Player === */
+const player = document.getElementById(`idPlayer`);
+const Video = player.querySelector(`.viewer`);
+const progress = player.querySelector(`.progress`);
+const progressBar = player.querySelector(`.progress__filled`);
+const toggle = player.querySelector(`.toggle`);
+const playerButtons = document.querySelectorAll(`.player__button`);
+const skipButtons = document.querySelectorAll(`[data-skip]`);
+const rangeSlider = document.querySelectorAll(`.player__slider`);
